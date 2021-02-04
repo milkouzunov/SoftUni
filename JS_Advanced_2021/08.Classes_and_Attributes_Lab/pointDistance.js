94c7386799bc3a8fa002a4eb6f312387d969cbfc
milko00 <milko.uzunov69@gmai.com>
2021-02-04 12:00:58 +0200

    classes

---
+++ JS_Advanced_2021/08.Classes_and_Attributes_Lab/pointDistance.js
@@ -0,0 +1,17 @@
+class Point {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(p1, p2) {
+        const dx = p1.x - p2.x;
+        const dy = p1.y - p2.y;
+        
+        return Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2))
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
