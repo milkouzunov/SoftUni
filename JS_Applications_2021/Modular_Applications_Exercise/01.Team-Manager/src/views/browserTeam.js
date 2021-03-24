e915f2f2ca9b3de04a3f98e211eb9934a3cd6d0a
Milko Uzunov <milko00@users.noreply.github.com>
2021-03-24 12:15:23 +0200

    ed

--- JS_Applications_2021/Modular_Applications_Exercise/01.Team-Manager/src/views/browserTeam.js
+++ JS_Applications_2021/Modular_Applications_Exercise/01.Team-Manager/src/views/browserTeam.js
@@ -1,7 +1,8 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
+import {getTeams} from '../api/data.js'
 
-const browserTemplate = () => html`<section id="browse">
+const browserTemplate = (teams) => html`<section id="browse">
 
     <article class="pad-med">
         <h1>Team Browser</h1>
@@ -11,39 +12,23 @@ const browserTemplate = () => html`<section id="browse">
         <div class="pad-small"><a href="#" class="action cta">Create Team</a></div>
     </article>
 
-    <article class="layout">
-        <img src="./assets/atat.png" class="team-logo left-col">
-        <div class="tm-preview">
-            <h2>Storm Troopers</h2>
-            <p>These ARE the droids we're looking for</p>
-            <span class="details">5000 Members</span>
-            <div><a href="#" class="action">See details</a></div>
-        </div>
-    </article>
-
-    <article class="layout">
-        <img src="./assets/rocket.png" class="team-logo left-col">
-        <div class="tm-preview">
-            <h2>Team Rocket</h2>
-            <p>Gotta catch 'em all!</p>
-            <span class="details">3 Members</span>
-            <div><a href="#" class="action">See details</a></div>
-        </div>
-    </article>
-
-    <article class="layout">
-        <img src="./assets/hydrant.png" class="team-logo left-col">
-        <div class="tm-preview">
-            <h2>Minions</h2>
-            <p>Friendly neighbourhood jelly beans, helping evil-doers succeed.</p>
-            <span class="details">150 Members</span>
-            <div><a href="#" class="action">See details</a></div>
-        </div>
-    </article>
+   ${teams.map(teamTemplate)}
 
 </section>`
 
+const teamTemplate = (team) => html`<article class="layout">
+<img src=${team.logoUrl} class="team-logo left-col">
+<div class="tm-preview">
+    <h2>${team.name}</h2>
+    <p>${team.description}</p>
+    <span class="details">? Members</span>
+    <div><a href=${`/details/${team._id}`} class="action">See details</a></div>
+</div>
+</article>`
+
 
 export async function browserTeam(ctx) {
-    ctx.render(browserTemplate());
+    const teams = await getTeams();
+
+    ctx.render(browserTemplate(teams));
 }
