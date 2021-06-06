7ff463394523947264ba1a894be640315db9d106
Milko Uzunov <milko00@users.noreply.github.com>
2021-06-06 22:59:36 +0300

    Cubicle

--- JS-Back-End/Cubicle-Workshop/config/setupMongoose.js
+++ JS-Back-End/Cubicle-Workshop/config/setupMongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://milko00:RqhfVMAjT1w2HerC@cluster0.rteqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb+srv://milko00:1m23m554mffds4@cluster0.h1cvf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
     
     const db = mongoose.connection;
 
